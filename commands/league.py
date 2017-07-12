3a3c5c6f4d1382db626f09c7114960fef32c0c27
ascott97 <alex.scott.java@gmail.com>
2017-07-12 21:30:18 +0100

    fix space/tab issue

--- commands/league.py
+++ commands/league.py
@@ -2,22 +2,22 @@ import requests
 import json
 import auth
 
-  
+
 class league:
 
     def __init__(self):
         self.api_key = auth.get_auth()['leagueoflegends']['api_key']
 
     def get_summoner_id(self, summoner_name):
-        url = 'https://euw.api.riotgames.com/api/lol/EUW/v1.4/summoner/by-name/'
-        params = {'api_key': self.api_key }    
+        url = 'https://euw1.api.riotgames.com/lol/summoner/v3/summoners/by-name/'
+        params = {'api_key': self.api_key }
         response = requests.get((url + summoner_name), params=params)
         response = json.loads(response.text)
-        summoner_id = response[summoner_name]['id']
-        return summoner_id
+        summoner_id = response['id']
+        return account_id
 
     def get_current_game(self, summoner_id):
-        url = 'https://euw.api.riotgames.com/observer-mode/rest/consumer/getSpectatorGameInfo/EUW1/'
+        url = 'https://euw1.api.riotgames.com/lol/spectator/v3/active-games/by-summoner'
         params = {'api_key': self.api_key }
         response = requests.get((url + str(summoner_id)), params=params)
         if response.status_code == 404:
@@ -27,14 +27,14 @@ class league:
             players = []
             for i in response['participants']:
                 players.append(i['summonerName'])
-            game = ('{:<30}        {:>30}\n'.format(players[0], players[5]) + 
-                    '{:<30}        {:>30}\n'.format(players[1], players[6]) + 
-                    '{:<30}   vs   {:>30}\n'.format(players[2], players[7]) + 
-                    '{:<30}        {:>30}\n'.format(players[3], players[8]) + 
+            game = ('{:<30}        {:>30}\n'.format(players[0], players[5]) +
+                    '{:<30}        {:>30}\n'.format(players[1], players[6]) +
+                    '{:<30}   vs   {:>30}\n'.format(players[2], players[7]) +
+                    '{:<30}        {:>30}\n'.format(players[3], players[8]) +
                     '{:<30}        {:>30}'.format(players[4], players[9])
                    )
         return game
-    
+
     def get_profile(self, summoner_id):
       url = 'https://euw.api.riotgames.com/placeholder'
       params = {'api_key': self.api_key}
@@ -56,6 +56,6 @@ class league:
             summoner_name = message.content.split[2]
             summoner_id = self.get_summoner_id(summoner_name)
             profile = self.get_profile(summoner_id)
-            return profile 
+            return profile
         else:
-	    return "Unkown command, must be one of `live` or `profile`"
+            return "Unkown command, must be one of `live` or `profile`"
